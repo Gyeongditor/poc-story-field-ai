51e04b67cee6dcc5daa2c2f347100555097811ef
kimchaneil <chaneil2@naver.com>
2025-06-27 15:56:47 +0900

    [Test] Huggingface story

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
 start_time = time.time()
 small_model = whisper.load_model('small',device=device)
-audio_path = f"./fairytale_test.mp3"
+#audio_path = f"./fairytale_test.mp3"
+audio_path = f"./travel_test.wav"
 result = small_model.transcribe(audio_path,language="ko",fp16=False)
 print(result['text'])
 
